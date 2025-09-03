c974e1371f90bc08797bdf8fc09098845643df7a
Mingrui <nicolezawa@outlook.com>
2025-09-03 19:36:03 +1000

    qmr9.3

--- Back-end/AITrip/AITrip/urls.py
+++ Back-end/AITrip/AITrip/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('acounts.urls')),
+    path('api/review/', include('review.urls')),
 ]
