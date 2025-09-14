20d8baaa932b6a28aa3fdfdcfca1135e166249b6
Pharus <771636307@qq.com>
2025-09-14 19:30:44 +1000

    update CRUD

--- Back-end/AITrip/itinerary/views.py
+++ Back-end/AITrip/itinerary/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render
 from rest_framework import viewsets, permissions
 from .models import Itinerary, DailySchedule, POI
 from .serializers import ItinerarySerializer, DailyScheduleSerializer, POISerializer
+from django_filters.rest_framework import DjangoFilterBackend
 
 from utils.response import CustomModelViewSet
 
@@ -11,6 +12,8 @@ class ItineraryViewSet(CustomModelViewSet):
     queryset = Itinerary.objects.all()
     serializer_class = ItinerarySerializer
     permission_classes = [permissions.IsAuthenticated]
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = '__all__'
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
@@ -19,8 +22,12 @@ class DailyScheduleViewSet(CustomModelViewSet):
     queryset = DailySchedule.objects.all()
     serializer_class = DailyScheduleSerializer
     permission_classes = [permissions.IsAuthenticated]
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = '__all__'
 
 class POIViewSet(CustomModelViewSet):
     queryset = POI.objects.all()
     serializer_class = POISerializer
     permission_classes = [permissions.IsAuthenticated]
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = '__all__'
