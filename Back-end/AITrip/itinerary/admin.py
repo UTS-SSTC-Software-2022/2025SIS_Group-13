331242939f9cb47ae45881107568973bc7644b2e
Pharus <771636307@qq.com>
2025-09-14 14:08:22 +1000

    update admin

--- Back-end/AITrip/itinerary/admin.py
+++ Back-end/AITrip/itinerary/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Itinerary, DailySchedule, POI
 
 # Register your models here.
+admin.site.register(Itinerary)
+admin.site.register(DailySchedule)
+admin.site.register(POI)
